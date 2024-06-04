29f983c5bd6df2009c52910a05558d4e26b1af7b
jonfo <jonf42@comcast.net>
2024-06-04 01:34:21 +0000

    challenge 02_03

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,8 +3,11 @@ package com.linkedin.javacodechallenges;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Objects;
 
 public class App {
   public static final Map<Character, Integer> letterPoints = Map.ofEntries(Map.entry('A', 1),
@@ -17,25 +20,17 @@ public class App {
       Map.entry('Z', 10));
 
   public static int wordScoreCalculator(String word) {
-    // Split up word into a list of 1 char strings
-    // List<Character> chars = new ArrayList<>();
-    List<Character> chars = word.chars().mapToObj(e -> (char) e)
+
+    // Create a set of valid keys.
+    Set<Character> validKeys = letterPoints.keySet();
+
+    // Split up word into a list of 1 char strings.
+    // Also, upcase the alphabetic chars.
+    List<Character> chars = word.toUpperCase().chars().mapToObj(e -> (char) e)
+        .filter(c -> validKeys.contains(c))
         .collect(Collectors.toList());
 
-    // for (char ch : word.toCharArray()) {
-    // chars.add(Character.toUpperCase(ch));
-    // }
-
-    final int points = 0;
-    System.out.println(letterPoints.get('Z'));
-    chars.forEach(ch -> System.out.println("word: " + ch));
-
-    // int points = chars.forEach()
-    // .filter(ch -> (App.letterPoints.containsKey(ch)))
-    // .collector(Collectors.summingInt());
-
-    // System.out.println("word = " + word + ":, " + word.chars());
-    return points;
+    return chars.stream().mapToInt(i -> letterPoints.get(i)).sum();
   }
 
   public static void main(String[] args) {
