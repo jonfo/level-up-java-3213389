6c0a8a94419f7513dc86ff2401c20d2cd7548f8d
jonfo <jonf42@comcast.net>
2024-06-06 19:48:58 +0000

    challenge 02_05

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,8 +3,19 @@ package com.linkedin.javacodechallenges;
 import java.util.List;
 
 public class App {
+
     public static double calculateAverageChangeInvested(List<Double> purchases) {
-        return 0;
+        double avgDiff = 0.0;
+        if (purchases.size() > 0) {
+            avgDiff = purchases.stream()
+                //.mapToDouble(d -> Math.ceil(d) - d).average().orElse(0);
+                .mapToDouble(d -> Math.ceil(d) - d).sum() / purchases.size();
+        }
+        /*
+        avgDiff = purchases.stream()
+                .mapToDouble(d -> Math.ceil(d) - d).average().orElse(0);
+        */       
+        return avgDiff;
     }
 
     public static void main(String[] args) {
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -7,30 +7,30 @@ import java.util.List;
 import org.junit.Test;
 
 public class AppTest {
-    @Test
-    public void calculateAverageChangeInvested_empty() {
-        assertEquals(0,
-                App.calculateAverageChangeInvested(List.of()), 0);
-    }
+        @Test
+        public void calculateAverageChangeInvested_empty() {
+                assertEquals(0,
+                                App.calculateAverageChangeInvested(List.of()), 0);
+        }
 
-    @Test
-    public void calculateAverageChangeInvested_oneItem() {
-        assertEquals(0.75,
-                App.calculateAverageChangeInvested(List.of(19.25)), 0);
-    }
+        @Test
+        public void calculateAverageChangeInvested_oneItem() {
+                assertEquals(0.75,
+                                App.calculateAverageChangeInvested(List.of(19.25)), 0);
+        }
 
-    @Test
-    public void calculateAverageChangeInvested() {
-        List<Double> purchases = List.of(12.38, 38.29, 5.27, 3.21);
-        double estimatedAverageChangeInvested = ((13 - 12.38) + (39 - 38.29) +
-                (6 - 5.27) + (4 - 3.21)) / 4;
-        assertEquals(estimatedAverageChangeInvested,
-                App.calculateAverageChangeInvested(purchases), 0);
+        @Test
+        public void calculateAverageChangeInvested() {
+                List<Double> purchases = List.of(12.38, 38.29, 5.27, 3.21);
+                double estimatedAverageChangeInvested = ((13 - 12.38) + (39 - 38.29) +
+                                (6 - 5.27) + (4 - 3.21)) / 4;
+                assertEquals(estimatedAverageChangeInvested,
+                                App.calculateAverageChangeInvested(purchases), 0);
 
-        purchases = List.of(19.99, 19.95, 29.95, 39.95, 19.99);
-        estimatedAverageChangeInvested = ((20 - 19.99) + (20 - 19.95) +
-                (30 - 29.95) + (40 - 39.95) + (20 - 19.99)) / 5;
-        assertEquals(estimatedAverageChangeInvested,
-                App.calculateAverageChangeInvested(purchases), 0);
-    }
+                purchases = List.of(19.99, 19.95, 29.95, 39.95, 19.99);
+                estimatedAverageChangeInvested = ((20 - 19.99) + (20 - 19.95) +
+                                (30 - 29.95) + (40 - 39.95) + (20 - 19.99)) / 5;
+                assertEquals(estimatedAverageChangeInvested,
+                                App.calculateAverageChangeInvested(purchases), 0);
+        }
 }
