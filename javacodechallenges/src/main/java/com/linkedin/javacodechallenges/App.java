8def547293839905c98fcdbf7a00901f3dc987ef
Jon Fo <jfo@drcloudemr.com>
2024-06-11 09:22:32 +0000

    challenge 03_03b .   Added a few JUnit tests.

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,37 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+//import java.util.Optional;
 
-public class App 
-{
-    public static void redactTextFile(String fileName,
-                                      String[] redactedWordsArray) {
-
+public class App {
+    public static List<String> redactTextFile(String fileName,
+            String[] redactedWordsArray) throws FileNotFoundException, IOException {
+        // Not sure if this is a legitimate use of FileNotFoundException...
+        if (fileName == null || fileName == "")
+            throw new FileNotFoundException("No filename specified");
+        /*
+         * System.out.println("File to redact: " + fileName);
+         * System.out.println("Redacted words list: " +
+         * Arrays.toString(redactedWordsArray));
+         */
+        ArrayList<String> redactedText = new ArrayList<>();
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        String rex = "(?i)" + String.join("|", redactedWordsArray);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String outLine = line.replaceAll(rex, "REDACTED");
+            redactedText.add(outLine);
+            // redactedText.forEach(s -> System.out.println("debug: " + s));
+        }
+        sc.close();
+        return redactedText;
     }
 
     public static void main(String[] args) {
@@ -24,8 +49,14 @@ public class App
         String redactedWords = scanner.nextLine();
         String[] redactedWordsList = redactedWords.split(",");
 
-        redactTextFile(fileName, redactedWordsList);
-
+        try {
+            List<String> result = redactTextFile(fileName, redactedWordsList);
+            result.forEach(s -> System.out.println("output: " + s));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
         scanner.close();
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,64 @@
 package com.linkedin.javacodechallenges;
 
 import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertThat;
+//import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    /**
+     * Test redaction no file
+     */
+    @Test
+    public void TestRedactionNoFile() {
+        try {
+            Path testFile = Files.createTempFile("jctest", ".txt");
+            String[] redactList = { "ivy", "oats" };
+            List<String> redactedList = App.redactTextFile(testFile.toAbsolutePath().toString(), redactList);
+            assertTrue(redactedList.size() == 0);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+            return;
+        }
+        assertTrue(true);
+    }
+
+    /**
+     * Test redaction
+     */
+    @Test
+    public void TestRedaction() {
+        try {
+            Path testFile = Files.createTempFile("jctest", ".txt");
+            Files.writeString(testFile, "Mares eat oats and does eat oats and little lambs eat ivy.\n" +
+                    "A kid'll eat ivy too, wouldn't you.");
+            String[] redactList = { "ivy", "oats" };
+            List<String> redactedList = App.redactTextFile(testFile.toAbsolutePath().toString(), redactList);
+            assertTrue(redactedList.size() != 0);
+            Files.delete(testFile);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+            return;
+        }
+        assertTrue(true);
     }
 }
